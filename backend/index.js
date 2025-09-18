b246a0b838e5b48252e2f327036a9a141cec229d
Shourya <shourya124sensei@gmail.com>
2025-09-18 23:18:29 +0530

    scheduling route

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import { app } from './app.js'
 import { connectDB } from './connection/mongodb.connect.js'
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const PORT=8000;
 
 connectDB().then(()=>{
-    app.listen(PORT,(req,res)=>{
+    app.listen(process.env.PORT || PORT,(req,res)=>{
         console.log(`Server Connected At ${PORT}`);
     })
     console.log('MongoDB connected')
