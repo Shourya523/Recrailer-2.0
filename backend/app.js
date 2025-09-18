7c1a9da33c7e08f7db1b0f0ce8a31325641d5c97
Shourya <shourya124sensei@gmail.com>
2025-09-18 23:33:41 +0530

    scheduling route

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,16 @@ import express from 'express'
 import { registerRouter } from './routes/register.route.js'
 import cors from 'cors'
 import "./Cron/scheduler.js";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CORS,
+  })
+);
 app.use('/api',registerRouter);
 
